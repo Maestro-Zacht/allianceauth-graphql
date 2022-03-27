d2e7fbdfea5a07ee4afbaffd97c190895356a0ec
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-03-27 11:53:23 +0200

    test

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Maestro-Zacht/allianceauth-graphql',
-    version='0.0.3',
+    version='0.1.0',
     zip_safe=False,
 )
