a3161036995341ed1f9ccaca6eb09ebdaf5b373c
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-04-06 15:44:34 +0200

    added verify email view

---
+++ allianceauth_graphql/authentication/views.py
@@ -0,0 +1,25 @@
+from django.core import signing
+from django.shortcuts import redirect, HttpResponse
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+
+REGISTRATION_SALT = getattr(settings, "REGISTRATION_SALT", "registration")
+
+
+def verify_email(request):
+    activation_key = request.GET.get('activation_key')
+    try:
+        dump = signing.loads(activation_key, salt=REGISTRATION_SALT, max_age=settings.ACCOUNT_ACTIVATION_DAYS * 86400)
+    except signing.BadSignature:
+        return HttpResponse("Invalid signature")
+
+    user = User.objects.get(pk=dump[0])
+    user.email = dump[1]
+    user.is_active = True
+    user.save()
+
+    site = getattr(settings, 'REDIRECT_SITE')
+    path = getattr(settings, 'REDIRECT_PATH', '/registration/callback/')
+
+    return redirect(site + path)
--- allianceauth_graphql/urls.py
+++ allianceauth_graphql/urls.py
@@ -3,10 +3,12 @@ from django.conf import settings
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from .schema import schema
+from .authentication.views import verify_email
 
 app_name = 'allianceauth_graphql'
 
 
 urlpatterns = [
     path("", csrf_exempt(GraphQLView.as_view(graphiql=getattr(settings, 'SHOW_GRAPHIQL', True), schema=schema)), name='graphql'),
+    path('verify/', verify_email, name='verify_email')
 ]
