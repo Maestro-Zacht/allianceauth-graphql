5ae6be426c2c04968f7bbbedff29fbcea9e94e4d
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-04-11 23:54:54 +0200

    added running averages + permissions on queries

--- allianceauth_graphql/community_creations/allianceauth_pve_integration/queries.py
+++ allianceauth_graphql/community_creations/allianceauth_pve_integration/queries.py
@@ -1,45 +1,44 @@
 import graphene
-from graphql_jwt.decorators import login_required
-
+from graphql_jwt.decorators import login_required, permission_required
 from graphene_django_extras import DjangoFilterPaginateListField, LimitOffsetGraphqlPagination
 
-from allianceauth_pve.models import Rotation
+from django.utils import timezone
+from django.db.models import OuterRef, Sum, Subquery
+from django.db.models.functions import Coalesce
 
-from .types import RotationType, EntryType
+from allianceauth_pve.models import Rotation, EntryCharacter
+
+from .types import RotationType, EntryType, RattingSummaryType
 
 
 class Query:
     rotation = graphene.Field(RotationType, id=graphene.Int(required=True))
     closed_rotations = graphene.List(RotationType)
-    # running_averages = graphene.List(CharacterStatsType, start_date=graphene.Date(required=True), end_date=graphene.Date())
-    # char_running_averages = graphene.Field(CharacterStatsType, start_date=graphene.Date(required=True), end_date=graphene.Date())
+    char_running_averages = graphene.Field(RattingSummaryType, start_date=graphene.Date(required=True), end_date=graphene.Date())
     active_rotations = graphene.List(RotationType)
     rotation_entries = DjangoFilterPaginateListField(EntryType, pagination=LimitOffsetGraphqlPagination(), fields=['rotation_id'])
 
+    @permission_required('allianceauth_pve.view_rotation')
     @login_required
     def resolve_rotation(self, info, id):
         return Rotation.objects.get(pk=id)
 
+    @permission_required('allianceauth_pve.view_rotation')
     @login_required
     def resolve_closed_rotations(self, info):
         return Rotation.objects.filter(is_closed=True).order_by('-closed_at')
 
-    # @login_required
-    # def resolve_running_averages(self, info, start_date, end_date=now().date()):
-    #     results = [
-    #         {
-    #             'character': char,
-    #             **RattingService.character_stats_task(char.character_id, start_date.isoformat(), end_date.isoformat())
-    #         } for char in CharacterService.get_all_instances()
-    #     ]
+    @login_required
+    def resolve_char_running_averages(self, info, start_date, end_date=timezone.now().date()):
+        char = info.context.user
+        rotations = Rotation.objects.filter(closed_at__range=(start_date, end_date)).get_setup_summary().filter(character_id=char).values('total_setups')
+        return EntryCharacter.objects.filter(entry__rotation__closed_at__range=(start_date, end_date), character=char)\
+            .values('character').order_by()\
+            .annotate(helped_setups=Coalesce(Subquery(rotations[:1]), 0))\
+            .annotate(estimated_total=Sum('estimated_share_total'))\
+            .annotate(actual_total=Sum('actual_share_total'))
 
-    #     return sorted(results, key=lambda k: k['total_earned'], reverse=True)
-
-    # @login_required
-    # def resolve_char_running_averages(self, info, start_date, end_date=now().date()):
-    #     char = info.context.user
-    #     return {'character': char, **RattingService.character_stats_task(char.character_id, start_date.isoformat(), end_date.isoformat())}
-
+    @permission_required('allianceauth_pve.view_rotation')
     @login_required
     def resolve_active_rotations(self, info):
         return Rotation.objects.filter(is_closed=False).order_by('-priority')
