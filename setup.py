91696e18591b1c26fc95219ff6d5c3eb995426e6
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-04-13 15:10:56 +0200

    Bump version: 0.5.8 → 0.6.0

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Maestro-Zacht/allianceauth-graphql',
-    version='0.5.8',
+    version='0.6.0',
     zip_safe=False,
 )
