2b839f2c89b495f6d381eb806aab23a71865618c
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-05-09 14:43:18 +0200

    updated inputs

--- allianceauth_graphql/community_creations/allianceauth_pve_integration/inputs.py
+++ allianceauth_graphql/community_creations/allianceauth_pve_integration/inputs.py
@@ -1,21 +1,29 @@
 import graphene
 
 
+class EntryRoleInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    value = graphene.Int(required=True)
+
+
 class EntryCharacterInput(graphene.InputObjectType):
     share_count = graphene.Int(required=True)
     user_id = graphene.Int(required=True)
+    character_id = graphene.Int(required=True)
     helped_setup = graphene.Boolean(default_value=False)
+    role = graphene.String(required=True)
 
 
 class EntryInput(graphene.InputObjectType):
     estimated_total = graphene.Float(required=True)
     shares = graphene.List(EntryCharacterInput, required=True)
+    roles = graphene.List(EntryRoleInput, required=True)
 
 
 class CreateRotationInput(graphene.InputObjectType):
     name = graphene.String(required=True)
-    tax_rate = graphene.Float(required=True)
-    priority = graphene.Int(default_value=0.0)
+    tax_rate = graphene.Float(default_value=0.0)
+    priority = graphene.Int(default_value=0)
 
 
 class RotationCloseInput(graphene.InputObjectType):
