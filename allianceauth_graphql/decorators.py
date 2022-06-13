a9bc0848934a0e18ae7fa956e0c62ad79e20a63d
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-06-13 20:43:14 +0200

    copied allianceauth any permission decorator

--- allianceauth_graphql/decorators.py
+++ allianceauth_graphql/decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from graphql_jwt.exceptions import PermissionDenied
-from graphql_jwt.decorators import context
+from graphql_jwt.decorators import context, user_passes_test
 
 from esi.models import Token
 
@@ -21,3 +21,30 @@ def tokens_required(scopes, exc=PermissionDenied):
                 raise exc('Required token missing')
         return _wrapped_func
     return decorator
+
+
+def permissions_required(perm, raise_exception=False):
+    """
+    Decorator for views that checks whether a user has a particular permission
+    enabled.
+    If the raise_exception parameter is given the PermissionDenied exception
+    is raised.
+
+    This decorator is the graphql version of the allianceauth permission_required.
+    """
+    def check_perms(user):
+        if isinstance(perm, str):
+            perms = (perm,)
+        else:
+            perms = perm
+
+        for perm_ in perms:
+            perm_ = (perm_,)
+            if user.has_perms(perm_):
+                return True
+
+        if raise_exception:
+            raise PermissionDenied
+
+        return False
+    return user_passes_test(check_perms)
