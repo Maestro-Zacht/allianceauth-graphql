e2b8fcf3c51dbbd3227eb6d725191a22653e710e
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-12-24 16:37:34 +0100

    more tests

--- allianceauth_graphql/tests/test_authentication.py
+++ allianceauth_graphql/tests/test_authentication.py
@@ -209,6 +209,85 @@ class TestEsiTokenAuthMutation(GraphQLTestCase):
         self.user.refresh_from_db()
         self.assertTrue(self.user.is_active)
 
+    @patch('esi.managers.TokenManager.create_from_code')
+    def test_token_equivalent_exists(self, mock_create_from_code):
+        newtoken = add_new_token(self.user, self.user.profile.main_character, "publicData")
+
+        mock_create_from_code.return_value = newtoken
+
+        response = self.query(
+            '''
+            mutation testM($sso_token: String!) {
+                tokenAuth(ssoToken: $sso_token) {
+                    me {
+                        id
+                    }
+                    errors
+                    status
+                }
+            }
+            ''',
+            operation_name='testM',
+            variables={'sso_token': 'nice_token'}
+        )
+
+        self.assertJSONEqual(
+            response.content,
+            {
+                'data': {
+                    'tokenAuth': {
+                        'errors': [],
+                        'status': LoginStatus.LOGGED_IN.name,
+                        'me': {
+                            'id': str(self.user.pk)
+                        }
+                    }
+                }
+            }
+        )
+
+        self.assertFalse(
+            Token.objects
+            .filter(pk=newtoken.pk)
+            .exists()
+        )
+
+    @patch('esi.managers.TokenManager.create_from_code')
+    def test_not_able_to_authenticate(self, mock_create_from_code):
+        mock_create_from_code.return_value = self.token
+
+        self.user.is_active = False
+        self.user.save()
+
+        response = self.query(
+            '''
+            mutation testM($sso_token: String!) {
+                tokenAuth(ssoToken: $sso_token) {
+                    me {
+                        id
+                    }
+                    errors
+                    status
+                }
+            }
+            ''',
+            operation_name='testM',
+            variables={'sso_token': 'nice_token'}
+        )
+
+        self.assertJSONEqual(
+            response.content,
+            {
+                'data': {
+                    'tokenAuth': {
+                        'errors': ['Unable to authenticate the selected character'],
+                        'status': LoginStatus.ERROR.name,
+                        'me': None
+                    }
+                }
+            }
+        )
+
 
 class TestRegistrationMutation(GraphQLTestCase):
     maxDiff = None
