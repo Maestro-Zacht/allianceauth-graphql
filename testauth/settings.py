faba3cfe921adccb1b0279d4a79c647e3011a4cd
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2024-01-03 21:12:31 +0100

    updated project

--- testauth/settings.py
+++ testauth/settings.py
@@ -13,24 +13,32 @@ from datetime import timedelta
 
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.humanize",
-    "django_celery_beat",
-    "bootstrapform",
-    "sortedm2m",
-    "esi",
-    "allianceauth",
-    "allianceauth.authentication",
-    "allianceauth.services",
-    "allianceauth.eveonline",
-    "allianceauth.groupmanagement",
-    "allianceauth.notifications",
-    "allianceauth.thirdparty.navhelper",
+    'allianceauth',  # needs to be on top of this list to support favicons in Django admin (see https://gitlab.com/allianceauth/allianceauth/-/issues/1301)
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django_celery_beat',
+    'bootstrapform',
+    'django_bootstrap5',  # https://github.com/zostera/django-bootstrap5
+    'sortedm2m',
+    'esi',
+    'allianceauth.framework',
+    'allianceauth.authentication',
+    'allianceauth.services',
+    'allianceauth.eveonline',
+    'allianceauth.groupmanagement',
+    'allianceauth.notifications',
+    'allianceauth.thirdparty.navhelper',
+    'allianceauth.analytics',
+    'allianceauth.menu',
+    'allianceauth.theme',
+    'allianceauth.theme.darkly',
+    'allianceauth.theme.flatly',
+    'allianceauth.theme.materia',
 ]
 
 SECRET_KEY = "wow I'm a really bad default secret key"
@@ -62,14 +70,16 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
+    'allianceauth.menu.middleware.MenuSyncMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'allianceauth.authentication.middleware.UserSettingsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = "allianceauth.urls"
